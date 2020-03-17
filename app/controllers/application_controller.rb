2932c4587afeb3c0c139fd053c5132c3f4fea7fe
Kevin <bubbagump2020@gmail.com>
2020-03-17 13:33:53 -0500

    Working on Heroku optimization and bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::API
 
+    def fallback_index_html
+        render :file => 'public/index.html'
+    end
+
     private
 
     def current_user
         @current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
     end
 
-    def fallback_index_html
-        render :file => 'public/index.html'
-    end
+    
 
 end

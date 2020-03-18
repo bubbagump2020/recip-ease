3512262c00948920aec81d3951279b7d57189dae
Kevin <bubbagump2020@gmail.com>
2020-03-18 16:28:13 -0500

    working on heroku deployment bugs

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create], param: :username
 
   resources :recipes, only: [:index, :create, :delete, :update ]
-  resources :ingredients, only: [ :create, :delete, :update ]
+  resources :ingredients, only: [:index, :create, :delete, :update ]
   
   post '/login', to: ("sessions#login")
   delete '/logout', to: ("sessions#logout")

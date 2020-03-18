047f7e8a6c39203f6689ab32848336936a7f3e77
Kevin <bubbagump2020@gmail.com>
2020-03-18 12:48:22 -0500

    working on heroku deployment bugs

--- recip-ease-frontend/src/components/HomePage/SignIn.js
+++ recip-ease-frontend/src/components/HomePage/SignIn.js
@@ -57,20 +57,20 @@ const SignIn = (props) => {
         const loggedInUser = await resultUser.json()
         console.log(loggedInUser.user_name)
         console.log(loginProps)
-        // if(loggedInUser.success){
-        //     sessionStorage.setItem('userToken', loggedInUser.token.session_id)
-        //     dispatch(authenticatedUser(loggedInUser))
-        //     loginProps.history.push(`/users/${loggedInUser.username}`)
-        //     const successToast = () => {
-        //         toast.success("Welcome!")
-        //     }
-        //     successToast()
-        // } else {
-        //     const failToast = () => {
-        //         toast.error(loggedInUser.errors, { position: toast.POSITION.TOP_CENTER })
-        //     }
-        //     failToast()
-        // }
+        if(loggedInUser.success){
+            sessionStorage.setItem('userToken', loggedInUser.token.session_id)
+            dispatch(authenticatedUser(loggedInUser))
+            loginProps.history.push(`/users/${loggedInUser.username}`)
+            const successToast = () => {
+                toast.success("Welcome!")
+            }
+            successToast()
+        } else {
+            const failToast = () => {
+                toast.error(loggedInUser.errors, { position: toast.POSITION.TOP_CENTER })
+            }
+            failToast()
+        }
     }
 
     const handlePasswordEntry = (e) => {

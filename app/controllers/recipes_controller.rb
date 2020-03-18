2a11b68c03817a85e31d9b04ed822dc68d049342
Kevin <bubbagump2020@gmail.com>
2020-03-18 15:53:51 -0500

    working on heroku deployment bugs

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,9 @@
 class RecipesController < ApplicationController
     
-    # def index
-    #     recipes = Recipe.all
-    #     render json: { recipes: recipes, current_user: @current_user}
-    # end
+    def index
+        recipes = Recipe.all
+        render json: { recipes: recipes }
+    end
 
     # def show
     #     recipe = Recipe.find_by(params[:id])
